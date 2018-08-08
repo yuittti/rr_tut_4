5874c790da5efdb0ab12f7531e131a31d19ff9e5
yuittti <kozachenkos@gmail.com>
2018-08-08 15:21:32 +0300

    Add store

--- src/middleware/checker.js
+++ src/middleware/checker.js
@@ -1,9 +1,16 @@
-import { ADD_TODO } from "../actions/todos";
+import { ADD_TODO, ADD_GOAL } from "../actions/todos";
 
 const checker = (store) => (next) => (action) => {
     if (
-        action.type = ADD_TODO &&
+        action.type === ADD_TODO &&
         action.todo.name.toLowerCase().indexOf('bitcoin') !== -1
+    ) {
+        return alert(`Nope. That's a bad idea`);
+    }
+
+    if (
+        action.type === ADD_GOAL &&
+        action.goal.name.toLowerCase().indexOf('bitcoin') !== -1
     ) {
         return alert(`Nope. That's a bad idea`);
     }
